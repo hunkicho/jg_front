175204ec33b6a2e6401e0eed7797ca595fb56392
hunkicho <jgchk4814@gmail.com>
2023-01-21 15:55:14 +0900

    feat(eslint, prettier): Init eslint, prettier
    Init eslint and prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+import eslintPlugin from 'vite-plugin-eslint';
 
-})
+export default defineNuxtConfig({
+  vite: {
+    plugins: [eslintPlugin()],
+  },
+});
