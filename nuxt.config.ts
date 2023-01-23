0d3fcfc0800293db191e1ce5836ded31d8a071a7
hunkicho <jgchk4814@gmail.com>
2023-01-23 19:46:46 +0900

    test(seo): Remove seo
    Remove seo to test.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,15 +5,4 @@ export default defineNuxtConfig({
   vite: {
     plugins: [eslintPlugin()],
   },
-  app: {
-    head: {
-      charset: 'utf-16',
-      viewport: 'width=500, initial-scale=1',
-      title: 'My App',
-      meta: [
-        // <meta name="description" content="My amazing site">
-        { name: 'description', content: 'My amazing site.' },
-      ],
-    },
-  },
 });
