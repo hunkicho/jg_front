409ada263fe29e8d80a1a0d6a35bb2b27d845b4e
hunkicho <jgchk4814@gmail.com>
2023-01-23 17:58:06 +0900

    feat: Add seo
    Add base seo code.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,15 @@ export default defineNuxtConfig({
   vite: {
     plugins: [eslintPlugin()],
   },
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      title: 'My App',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: 'My amazing site.' },
+      ],
+    },
+  },
 });
