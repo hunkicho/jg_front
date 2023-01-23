12ab76a5544c3d739b04d27e5864ec6b8fa4b952
hunkicho <jgchk4814@gmail.com>
2023-01-23 20:20:31 +0900

    test(seo): Test Open Graph
    Upload test Open Graph code.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,17 @@ export default defineNuxtConfig({
   vite: {
     plugins: [eslintPlugin()],
   },
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      title: 'My App',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: 'My amazing site.' },
+        { name: 'og:title', content: '오지 메타테그'},
+        { name: 'og:description', content: '오지 디스크립션'}
+      ],
+    },
+  },
 });
